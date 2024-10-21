e13b5ab6488dc953376b3862ea76b2bbe00f743a
Andrii Zakharenko <andriizakharenko@gmail.com>
2024-10-21 09:45:22 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+if (thumbs && largeImg) {
+  thumbs.addEventListener('click', handleThumbnailClick);
+}
+
+function handleThumbnailClick(e) {
+  e.preventDefault();
+
+  const link = e.target.closest('.list-item__link');
+
+  if (link) {
+    updateLargeImage(link.href);
+  }
+}
+
+function updateLargeImage(src) {
+  largeImg.src = src;
+}
