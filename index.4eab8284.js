9536140c71979aff79b68fabd5f1bdde9fb53ace
Andrii Zakharenko <andriizakharenko@gmail.com>
2024-10-21 09:45:45 +0300

    make build

---
+++ index.4eab8284.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg"),t=document.getElementById("thumbs");t&&e&&t.addEventListener("click",function(t){t.preventDefault();var n,l=t.target.closest(".list-item__link");l&&(n=l.href,e.src=n)});
+//# sourceMappingURL=index.4eab8284.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,14 @@
+"use strict";
+const largeImg = document.getElementById("largeImg");
+const thumbs = document.getElementById("thumbs");
+if (thumbs && largeImg) thumbs.addEventListener("click", handleThumbnailClick);
+function handleThumbnailClick(e) {
+    e.preventDefault();
+    const link = e.target.closest(".list-item__link");
+    if (link) updateLargeImage(link.href);
+}
+function updateLargeImage(src) {
+    largeImg.src = src;
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
